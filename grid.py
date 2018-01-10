8412f88649d77e4fa382280902cdecfd1d03476d
goungy <jdubois69@gmail.com>
2018-01-10 22:20:00 +0100

    Added comments in grid class
    Added comments in grid class

--- grid.py
+++ grid.py
@@ -6,13 +6,18 @@ from subgrid import Subgrid
 class SudokuGrid (object):
 
     def __init__(self, file_to_open):
+        # Creation of cells list in 1D
         self.cases = [Case(int(i / 9), i % 9) for i in range(81)]
+        # Creation of 9 lists of references to cells corresponding to 9 lines
         self.lines = []
         for l in range(9):
             self.lines.append(self.cases[l*9:(l+1)*9])
+        # Creation of 9 lists of references to cells corresponding to 9 columns
         self.columns = []
         for c in range(9):
             self.columns.append(self.cases[c:81:9])
+        # Creation of 9 lists corresponding referencing cells to 9 blocks of 3x3
+        # Cells of Sudoku table
         self.subgrids = []
         for c in range(3):
             subgrid_line = []
